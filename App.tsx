7a6f4fb31570d2bbf569b9d02096e1b319315d37
engineerStuardo <ingeniero.stuardo@gmail.com>
2023-12-03 23:40:09 -0600

    adding splash screen to android

--- App.tsx
+++ App.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {Navigation} from './navigation';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import SplashScreen from 'react-native-splash-screen';
+import {Platform} from 'react-native';
 
 function App(): JSX.Element {
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      SplashScreen.hide();
+    }
+  }, []);
+
   return (
     <SafeAreaProvider>
       <NavigationContainer>
